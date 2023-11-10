7c7879fcae4ab467669ff29045c386dd4647211a
김성태 <39698079+kariseio@users.noreply.github.com>
2023-11-10 09:49:11 +0900

    Feat: 혼잡도 서비스 프로젝트 생성

---
+++ congestion-service/src/main/java/com/sth/congestionservice/CongestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sth.congestionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongestionServiceApplication.class, args);
+	}
+
+}
---
+++ congestion-service/src/test/java/com/sth/congestionservice/CongestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sth.congestionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
