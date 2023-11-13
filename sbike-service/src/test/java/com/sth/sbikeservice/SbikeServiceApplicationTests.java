ed3286fda047ccfd0551b78d3bc245ed1c58bb87
김성태 <39698079+kariseio@users.noreply.github.com>
2023-11-13 15:09:49 +0900

    Feat: 문화행사, 따릉이, 컨피그 프로젝트 생성

---
+++ sbike-service/src/test/java/com/sth/sbikeservice/SbikeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sth.sbikeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbikeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
