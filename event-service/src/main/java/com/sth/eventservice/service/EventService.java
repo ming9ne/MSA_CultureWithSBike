e9e460b5c4c415e95b234eb3b584c7fec10580ef
NORTH205-30\북205_130 <lcecoffee@naver.com>
2023-11-23 16:48:39 +0900

    11.23 집가서 추가작업 해야됨

--- event-service/src/main/java/com/sth/eventservice/service/EventService.java
+++ event-service/src/main/java/com/sth/eventservice/service/EventService.java
@@ -4,17 +4,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sth.eventservice.model.dto.EventDTO;
 import com.sth.eventservice.model.entity.Event;
 import com.sth.eventservice.repository.EventRepository;
+import com.sth.eventservice.vo.AreaResponse;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class EventService {
@@ -27,52 +30,61 @@ public class EventService {
         this.eventRepository = eventRepository;
         this.restTemplate = restTemplate;
 
-        // Add MappingJackson2HttpMessageConverter to support JSON mapping
+        // JSON 매핑을 지원하기 위해 MappingJackson2HttpMessageConverter 추가
         restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
     }
 
     @Transactional
-    public void callAreaService() {
+    public void callAreaServiceAndSaveEvents() {
         String areaServiceUrl = "http://localhost:8000/api/v1/area-service/areas";
 
-        ResponseEntity<List<Map<String, String>>> responseEntity = restTemplate.exchange(
-                areaServiceUrl,
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<Map<String, String>>>() {}
-        );
+        // 외부 API에서 지역 목록을 가져옴
+        AreaResponse[] areas = restTemplate.getForObject(areaServiceUrl, AreaResponse[].class);
 
-        if (responseEntity.getStatusCode().is2xxSuccessful()) {
-            List<Map<String, String>> areaList = responseEntity.getBody();
-            saveEventsFromAreaList(areaList);
-        } else {
-            System.out.println("Area Service 호출이 실패했습니다. 상태 코드: " + responseEntity.getStatusCodeValue());
+        System.out.println("받아온 지역 목록:");
+        for (AreaResponse area : areas) {
+            System.out.println(area.getAreaNm());
         }
-    }
 
-    @Transactional
-    public void saveEventsFromAreaList(List<Map<String, String>> areaList) {
-        for (Map<String, String> areaData : areaList) {
-            String area = areaData.get("areaNm");
+        // 지역 목록을 순회
+        for (int i = 0; i < areas.length; i++) {
+            int startPage = 1;
+            int pageSize = 100;
 
-            String apiUrl = "http://openapi.seoul.go.kr:8088/636f62694e6f757236364b49674759/xml/citydata/1/5/" + area;
+            // 외부 API에 사용될 URL 생성
+            String apiUrl = "http://openapi.seoul.go.kr:8088/636f62694e6f757236364b49674759/xml/citydata/"
+                    + startPage + "/" + pageSize + "/" + areas[i].getAreaNm();
 
             try {
-                ResponseEntity<CITYDATA> responseEntity = restTemplate.getForEntity(apiUrl, CITYDATA.class);
+                // XML 요청을 위한 헤더 설정
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_XML);
+                HttpEntity<String> entity = new HttpEntity<>(headers);
 
+                // 외부 API 호출
+                ResponseEntity<CITYDATA> responseEntity = restTemplate.exchange(
+                        apiUrl,
+                        HttpMethod.GET,
+                        entity,
+                        new ParameterizedTypeReference<CITYDATA>() {}
+                );
+
+                // 외부 API 호출이 성공한 경우
                 if (responseEntity.getStatusCode().is2xxSuccessful()) {
                     CITYDATA citydata = responseEntity.getBody();
 
-                    if (citydata != null && citydata.getEventSttsList() != null && !citydata.getEventSttsList().isEmpty()) {
-                        List<EVENT_STTS> eventSttsList = citydata.getEventSttsList();
+                    // 가져온 이벤트 데이터 처리
+                    if (citydata != null && citydata.getEventSttsWrapper() != null && citydata.getEventSttsWrapper().getEventSttsList() != null) {
+                        List<EVENT_STTS> eventSttsList = citydata.getEventSttsWrapper().getEventSttsList();
 
                         for (EVENT_STTS eventStts : eventSttsList) {
                             EventDTO eventDTO = new EventDTO();
                             eventDTO.setAREA_NM(citydata.getAreaNm());
                             eventDTO.setEVENT_NM(eventStts.getEventNm());
 
-                            saveEventsToDatabase(eventDTO);
-                            System.out.println("API 호출 성공 - 지역: " + area + ", 이벤트: " + eventStts.getEventNm());
+                            // 이벤트를 데이터베이스에 저장
+                            saveEventsToDatabase(List.of(eventDTO));
+                            System.out.println("API 호출 성공 - 지역: " + areas[i].getAreaNm() + ", 이벤트: " + eventStts.getEventNm());
                         }
                     } else {
                         System.out.println("API 응답에서 이벤트 정보를 찾을 수 없습니다");
@@ -86,9 +98,11 @@ public class EventService {
     }
 
     @Transactional
-    public void saveEventsToDatabase(EventDTO eventDTO) {
-        Event event = eventDTO.toEntity();
-        eventRepository.save(event);
+    public void saveEventsToDatabase(List<EventDTO> eventDTOList) {
+        for (EventDTO eventDTO : eventDTOList) {
+            Event event = eventDTO.toEntity();
+            eventRepository.save(event);
+        }
     }
 
     static class CITYDATA {
@@ -96,11 +110,20 @@ public class EventService {
         private String areaNm;
 
         @JsonProperty("EVENT_STTS")
-        private List<EVENT_STTS> eventSttsList;
+        private EVENT_STTSWrapper eventSttsWrapper;
 
         public String getAreaNm() {
             return areaNm;
         }
+
+        public EVENT_STTSWrapper getEventSttsWrapper() {
+            return eventSttsWrapper;
+        }
+    }
+
+    static class EVENT_STTSWrapper {
+        @JsonProperty("EVENT_STTS")
+        private List<EVENT_STTS> eventSttsList;
 
         public List<EVENT_STTS> getEventSttsList() {
             return eventSttsList;
