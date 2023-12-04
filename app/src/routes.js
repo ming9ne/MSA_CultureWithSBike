6eb791584f619bcef8d7c1eec5ced028cb8efc6b
Administrator <kariseio@naver.com>
2023-12-04 10:03:50 +0900

    Fix: 리스트 파일명 수정

--- app/src/routes.js
+++ app/src/routes.js
@@ -3,7 +3,7 @@ import Profile from "views/examples/Profile.js";
 import Maps from "views/examples/Maps.js";
 import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
-import Tables from "views/examples/Tables.js";
+import Lists from "views/examples/List.js";
 import Icons from "views/examples/Icons.js";
 
 var routes = [
@@ -22,10 +22,10 @@ var routes = [
     layout: "/admin",
   },
   {
-    path: "/tables",
-    name: "Tables",
+    path: "/lists",
+    name: "Lists",
     icon: "ni ni-bullet-list-67 text-red",
-    component: <Tables />,
+    component: <Lists />,
     layout: "/admin",
   },
   {
